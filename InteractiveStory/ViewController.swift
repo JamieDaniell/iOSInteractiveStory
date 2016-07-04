46d537c57c7f725032920b833a9553fcafd8e669
JamieDaniell <J@mieDaniell.com>
2016-07-04 16:45:58 +0100

    Model Refactor , Textfield animated

--- InteractiveStory/ViewController.swift
+++ InteractiveStory/ViewController.swift
@@ -8,15 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController
+class ViewController: UIViewController , UITextFieldDelegate
 {
-
+    enum Error: ErrorType
+    {
+        case NoName
+    }
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var textFieldBottomConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ViewController.keyboardWillShow(_:)), name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ViewController.keyboardWillHide(_:)), name: UIKeyboardWillHideNotification, object: nil)
         
-
     }
 
     override func didReceiveMemoryWarning()
@@ -28,12 +37,66 @@ class ViewController: UIViewController
     {
         if segue.identifier == "startAdventure"
         {
-            if let PageController = segue.destinationViewController as? PageController
+            
+            do
             {
-                PageController.page = Adventure.story
+                if  let name = nameTextField.text
+                {
+                    if name == ""
+                    {
+                        throw Error.NoName
+                    }
+                    if let PageController = segue.destinationViewController as? PageController
+                    {
+                        PageController.page = Adventure.story(name)
+                    }
+                }
+            }
+            catch Error.NoName
+            {
+                let alertController = UIAlertController(title: "Name Not Provided", message: "Provide a name to start a story!", preferredStyle: .Alert)
+                let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
+                alertController.addAction(action)
+                
+                presentViewController(alertController , animated: true , completion: nil)
+            }
+            catch let error
+            {
+                fatalError("\(error)")
             }
         }
     }
+    func keyboardWillShow(notification: NSNotification)
+    {
+        if let userInfoDict = notification.userInfo , keyboardFrameValue = userInfoDict[UIKeyboardFrameEndUserInfoKey] as? NSValue
+        {
+            let keyboardFrame = keyboardFrameValue.CGRectValue()
+            
+            UIView.animateWithDuration(0.8)
+            {
+                self.textFieldBottomConstraint.constant = keyboardFrame.size.height + 10
+                self.view.layoutIfNeeded()
+            }
+        }
+        
+    }
+    
+    func keyboardWillHide(notification: NSNotification)
+    {
+        UIView.animateWithDuration(0.8)
+        {
+            self.textFieldBottomConstraint.constant = 48
+            self.view.layoutIfNeeded()
+        }
+    }
+    //MARK - UITextFieldDelegate
+    func textFieldShouldReturn(textField: UITextField) -> Bool
+    {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    
 
 
 }
